e34295a0cec62349e787ebe1a8c8b3c6b109a327
maxuser74 <maxpasseri@libero.it>
2024-03-10 09:19:12 +0100

    Commit

--- todo.js
+++ todo.js
@@ -1,49 +1,24 @@
-const form = document.getElementById("todo-form");
-const input = document.getElementById("todo-input");
 const todoLane = document.getElementById("todo-lane");
+const doingLane = document.getElementById("doing-lane");
+const doneLane = document.getElementById("done-lane");
+
 const add_btn = document.getElementById("plus_btn");
 var elements = document.getElementsByClassName("task");
+var todo_elements = [];
+var doing_elements = [];
+var done_elements = [];
 
-/*
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const value = input.value;
-
-  if (!value) return;
-
-  const newTask = document.createElement("p");
-  newTask.classList.add("task");
-  newTask.setAttribute("draggable", "true");
-  newTask.innerText = value;
-
-  newTask.addEventListener("dragstart", () => {
-    newTask.classList.add("is-dragging");
-  });
-
-  newTask.addEventListener("dragend", () => {
-    newTask.classList.remove("is-dragging");
-  });
-
-  todoLane.appendChild(newTask);
-
-  input.value = "";
-});
-
-*/
-
-
-var myOnChanged = function() {
+var myOnChanged = function () {
   this.style.height = "auto";
   this.style.height = this.scrollHeight + "px";
-
 };
 
-function add_task(){
+function add_task() {
   const newTask = document.createElement("textarea");
   newTask.classList.add("task");
   newTask.setAttribute("draggable", "true");
   newTask.setAttribute("rows", "1");
-  
+
   newTask.innerText = "new task";
 
   newTask.addEventListener("dragstart", () => {
@@ -55,15 +30,59 @@ function add_task(){
   });
 
   todoLane.appendChild(newTask);
-
-};
+}
 
 for (var i = 0; i < elements.length; i++) {
- elements[i].addEventListener('input', myOnChanged, false);
- elements[i].style.height = "auto";
- elements[i].style.height = elements[i].scrollHeight + "px";
- // console.log(elements[i].scrollHeight);
-};
+  elements[i].addEventListener("input", myOnChanged, false);
+  elements[i].style.height = "auto";
+  elements[i].style.height = elements[i].scrollHeight + "px";
+}
 
 add_btn.addEventListener("click", add_task);
 
+function populate_data() {
+  todo_elements = [];
+  doing_elements = [];
+  done_elements = [];
+
+  // TODO:
+  var children = todoLane.children;
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].tagName === "TEXTAREA") {
+      todo_elements.push(children[i].innerHTML);
+    }
+  }
+
+  // DOING:
+  var children = doingLane.children;
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].tagName === "TEXTAREA") {
+      doing_elements.push(children[i].innerHTML);
+    }
+  }
+
+  // DONE:
+  var children = doneLane.children;
+  for (var i = 0; i < children.length; i++) {
+    if (children[i].tagName === "TEXTAREA") {
+      done_elements.push(children[i].innerHTML);
+    }
+  }
+
+  console.log("TODO Elements", todo_elements);
+  console.log("DOING Elements", doing_elements);
+  console.log("DONE Elements", done_elements);
+}
+
+todoLane.addEventListener("drop", () => {
+  console.log("TODO");
+  populate_data();
+});
+
+doingLane.addEventListener("drop", () => {
+  populate_data();
+});
+
+doneLane.addEventListener("drop", () => {
+  populate_data();
+});
